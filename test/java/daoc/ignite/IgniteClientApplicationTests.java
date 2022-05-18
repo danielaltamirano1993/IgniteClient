82deda9013c275ef102a6a13ed6ef17fa001ac7b
danielaltamirano1993 <64813513+danielaltamirano1993@users.noreply.github.com>
2022-05-17 23:29:52 -0500

    Create IgniteClientApplicationTests.java

---
+++ test/java/daoc/ignite/IgniteClientApplicationTests.java
@@ -0,0 +1,13 @@
+package daoc.ignite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgniteClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
